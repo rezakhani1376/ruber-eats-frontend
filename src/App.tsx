63508da9d091c8f80754d44c98553b200f841f10
reza khani <reza.khani1998@gmail.com>
2022-01-01 00:25:26 +0330

    Apollo Setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 
-
 function App() {
-  return (
-    <div className="bg-black">
-     <h1> Hollo from tailwindCss
-     </h1>
-    </div>
-  );
+	return (
+		<div className="bg-red-600">
+			<h1 className='bg-blue-500 hover:bg-green-300  '> Hollo from tailwindCSS</h1>
+		</div>
+	);
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,17 @@
+import { ApolloProvider } from '@apollo/client';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { client } from './apollo';
 
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import "./styles/output.css"
+import "./styles/tailwind.css"
 
 ReactDOM.render(
   <React.StrictMode>
+    <ApolloProvider client={client}>
     <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
